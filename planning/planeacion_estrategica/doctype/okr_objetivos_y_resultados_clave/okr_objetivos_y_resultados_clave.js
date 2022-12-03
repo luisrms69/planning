7ee079d66d44cf05ddcbec54fabb7498850a23e7
Luis Montanaro Developer env <admin@appcity.mx>
2022-12-02 23:04:55 -0600

    Se crean los doctypes iniciales, aun no hay codigo, unicamente un esqueleto

---
+++ planning/planeacion_estrategica/doctype/okr_objetivos_y_resultados_clave/okr_objetivos_y_resultados_clave.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, buzola and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('OKR Objetivos y Resultados Clave', {
+	// refresh: function(frm) {
+
+	// }
+});
